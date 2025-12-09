b00ee9abe9f7b11475c7a3c48453acb176b696a1
SIVA <darkbutterflystar31@gmail.com>
2025-12-10 02:38:21 +0530

    updated base url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "/Weather-App",
   resolve: {
     alias: {
       "@api": path.resolve(__dirname, "src/api"),
